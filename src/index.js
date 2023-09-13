9a5d0eafaad263795bc5658a8d6985ff4f81ce9b
PhilippAuch <philipp.auch@web.de>
2023-09-13 10:28:45 +0200

    more exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-console.log('Welcome to dispatcher.js');
-
 export function init() {
   console.log('dispatcher.js is now initializing');
 }
 
 export { fine, debug, info, warn, error, errorWithTicket } from './util/logging.js';
+export { AE, AMPERSAND, APOSTROPHE, EQUALS, HASHTAG, QUOTATION, SPACE, TILDE } from './util/JQLHelper.js';
