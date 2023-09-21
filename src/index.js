fd2c978ce685b60d2ff77f0e57ffbfed647fed3a
PhilippAuch <philipp.auch@web.de>
2023-09-21 10:35:28 +0200

    (feat) switch some back

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ export function init() {
   console.log('dispatcher.js is now initializing');
 }
 
-export { fine, debug, info, warn, error, errorWithTicket } from './util/logging.js';
-export { AE, AMPERSAND, APOSTROPHE, EQUALS, HASHTAG, QUOTATION, SPACE, TILDE } from './util/JQLHelper.js';
+export { fine, debug, info, warn, error, errorWithTicket, TicketError } from './util/logging.js';
+export { SPACE, QUOTATION, HASHTAG, AMPERSAND, APOSTROPHE, BRACKET_OPEN, BRACKET_CLOSE, SMALLER, EQUALS, TILDE, AE } from './util/HTMLHelper.js';
 export { sleep, pad, waitSyncMS } from './util/generalUtil.js';
+export { dateToIsoString, dateToICALString, dateToJQLString, getDateByDayAndSetPos, getNextWeekday, getPreviousWeekday, getDateFromString, getStringFromDate, cloneDate } from './util/dateHelper.js';
