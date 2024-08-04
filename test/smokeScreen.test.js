ae5a950401d841da77d823f2dd847e108c05ddf1
PhilippAuch <philipp.auch@web.de>
2024-08-04 09:34:59 +0200

    add test for init method

--- test/smokeScreen.test.js
+++ test/smokeScreen.test.js
@@ -1,7 +1,15 @@
 import * as assert from 'assert';
+import { init } from '../src/index.js';
 
 describe('Smoke Screen', () => {
   it('Test startup', () => {
+    assert.equal('s', 's');
+  });
+
+  function dummy() {}
+
+  it('Test init', () => {
+    init(dummy);
     assert.equal('s', 's');
   });
 });
